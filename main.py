e83519fc7bf33b8845cc8544c0d62e2d505f3a6c
kc2191 <73376671+kc2191@users.noreply.github.com>
2021-02-04 20:57:18 -0800

    frequently visited webbrowsing pages

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import webbrowser
 
-webbrowswer.open_new(https://mail.google.com/mail/u/0/#inbox, new=0 autoraise=True)
+url = 'http://docs.python.org/'
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+#private email
+#webbrowswer.open_new(mail.google.com/mail/u/0/#inbox)
+
+#school email
+#(https://mail.google.com/mail/u/1/#inbox)
+
+#proudmary coffee
+#https://proudmarycoffee.com/collections/all-coffee
+
+#poetrycollection
+#https://drive.google.com/drive/u/0/folders/1qBe4SggQ-2syCERrBpL5QsRrLPgOoj-R
+
+#ufcnews
+#https://twitter.com/ufcnews?lang=en
+
+#canvas
+#https://canvas.humboldt.edu/
+
+
+
