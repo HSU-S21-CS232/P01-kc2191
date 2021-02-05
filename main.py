764ae634a4573c9fac4cbba01a8e34196018d447
kc2191 <73376671+kc2191@users.noreply.github.com>
2021-02-04 21:14:26 -0800

    working but too simple

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import webbrowser
+import bs4
 
-url = 'http://docs.python.org/'
-chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+#r= requests.get()
+
+
+#url = 'http://docs.python.org/'
+#chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 
 #private email
-#webbrowswer.open_new(mail.google.com/mail/u/0/#inbox)
+webbrowswer.open('https://mail.google.com/mail/u/0/#inbox')
 
 #school email
-#(https://mail.google.com/mail/u/1/#inbox)
+webbrowser.open('https://mail.google.com/mail/u/1/#inbox')
 
 #proudmary coffee
-#https://proudmarycoffee.com/collections/all-coffee
+webbrowser.open('https://proudmarycoffee.com/collections/all-coffee')
 
 #poetrycollection
-#https://drive.google.com/drive/u/0/folders/1qBe4SggQ-2syCERrBpL5QsRrLPgOoj-R
+webbrowser.open('https://drive.google.com/drive/u/0/folders/1qBe4SggQ-2syCERrBpL5QsRrLPgOoj-R')
 
 #ufcnews
-#https://twitter.com/ufcnews?lang=en
+webbrowser.open('https://twitter.com/ufcnews?lang=en')
 
 #canvas
-#https://canvas.humboldt.edu/
+webbrowser.open('https://canvas.humboldt.edu/')
 
 
 
