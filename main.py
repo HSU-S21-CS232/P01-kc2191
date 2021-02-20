a53d3a3b11838ca6f8c1480c96cac8d8d0d8962d
kc2191 <73376671+kc2191@users.noreply.github.com>
2021-02-19 22:29:52 -0800

    final draft p02

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import webbrowser
+import webbrowser
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
@@ -32,22 +32,22 @@ f.close()
 
 
 #private email
-#webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
+webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
 
 #school email
-#webbrowser.open('https://mail.google.com/mail/u/1/#inbox')
+webbrowser.open('https://mail.google.com/mail/u/1/#inbox')
 
 #proudmary coffee
-#webbrowser.open('https://proudmarycoffee.com/collections/all-coffee')
+webbrowser.open('https://proudmarycoffee.com/collections/all-coffee')
 
 #poetrycollection
-#webbrowser.open('https://drive.google.com/drive/u/0/folders/1qBe4SggQ-2syCERrBpL5QsRrLPgOoj-R')
+webbrowser.open('https://drive.google.com/drive/u/0/folders/1qBe4SggQ-2syCERrBpL5QsRrLPgOoj-R')
 
 #ufcnews
-#webbrowser.open('https://twitter.com/ufcnews?lang=en')
+webbrowser.open('https://twitter.com/ufcnews?lang=en')
 
 #canvas
-#webbrowser.open('https://canvas.humboldt.edu/')
+webbrowser.open('https://canvas.humboldt.edu/')
 
 
 
