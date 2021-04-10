f08bef36fb43d4f5a8a695a6d18d1a8eb089bc9d
kc2191 <73376671+kc2191@users.noreply.github.com>
2021-04-10 16:59:07 -0700

    stuck at this point. will ask proff carter for help on Monday

--- p05-kc2191/project5.py
+++ p05-kc2191/project5.py
@@ -1,5 +1,15 @@
-from flask import flask
-app = FLask(__name__)
+from flask import Flask
+import os
+
+#tell flask what app to run
+os.environ["FLASK_APP"] = "project5.py"
+app = Flask(__name__)
 
 #base route (home page)
-@app.route(./.)
+@app.route('/')
+def home():
+    return '<h1>Hello, World! </h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Me</h1><p>My name is Kevin </p>'
